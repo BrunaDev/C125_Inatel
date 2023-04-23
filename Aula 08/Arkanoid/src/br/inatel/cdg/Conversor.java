c6ef03af476b631d5c9bc30fa126149ce02e5bb6
Bruna <72671246+BrunaDev@users.noreply.github.com>
2023-04-23 14:25:37 -0300

    delete conversor

--- Aula 08/Arkanoid/src/br/inatel/cdg/Conversor.java
+++
@@ -1,9 +0,0 @@
-package br.inatel.cdg;
-
-public class Conversor {
-
-    public static void converter(int pontos) {
-        System.out.println("Pontuação do jogador: " + pontos);
-        System.out.println("Moedas adquiridas: " + (pontos * 100));
-    }
-}
