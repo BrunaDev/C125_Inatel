028f2470ea3fa3d58ff7f9adb46cf9a90e6f29c7
Bruna <72671246+BrunaDev@users.noreply.github.com>
2023-05-28 21:45:38 -0300

    Update Usuario.java

--- Redes Sociais/src/br/inatel/cdg/Usuario.java
+++ Redes Sociais/src/br/inatel/cdg/Usuario.java
@@ -1,11 +1,5 @@
 package br.inatel.cdg;
 
-// Importações de classes de pacotes diferentes
-import br.inatel.cdg.redes.Facebook;
-import br.inatel.cdg.redes.GooglePlus;
-import br.inatel.cdg.redes.Instagram;
-import br.inatel.cdg.redes.Twitter;
-
 public class Usuario
 {
     public static String separator = "\n======================//====================\n"; // usado para criar um espaço no prompot para melhor
@@ -13,59 +7,38 @@ public class Usuario
     private String nome;
     private String email;
 
+    // Usado para armazenar as redes sociais do usuario
     RedeSocial[] redeSocial = new RedeSocial[5]; // 5 para ter um extra das 4 redes sociais
 
     // Construtor da classe
-    public void Usuario (RedeSocial redeSociais) {
+    public Usuario (RedeSocial[] redeSociais) {
 
         for (int i = 0; i < redeSocial.length; i++) {
 
-            //inserindo o array
+            //Se redeSocial for vazio, entra na ação
             if (redeSocial[i] == null) {
-                redeSocial[i] = redeSociais;
-
-                //vericando de qual classe pertence o tipo rede social
-                if (redeSocial[i] instanceof Facebook)
-                {
-                    System.out.println("O usuário foi cadastrado no Facebook!");
-                    break;
-                }
-                else if (redeSocial[i] instanceof Instagram)
-                {
-                    System.out.println("O usuário foi cadastrado no Instagram!");
-                    break;
-                }
-                else if (redeSocial[i] instanceof GooglePlus)
-                {
-                    System.out.println("O usuário foi cadastrado no GooglePlus!");
-                    break;
-                }
-                else if (redeSocial[i] instanceof Twitter)
-                {
-                    System.out.println("O usuário foi cadastrado no Twitter!");
-                    break;
-                }
-
+                redeSocial[i] = redeSociais[i];
                 break;
+            } else {
+                System.out.println("Não é mais possível cadastrar. Limite esgotado.");
             }
         }
     }
 
-
     //Getters e setters da classe
-    public String getNome() {
-        return nome;
-    }
-
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public String getEmail(){
+        return this.email;
     }
 }
