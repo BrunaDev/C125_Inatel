592bdf7091c1d9e29ad1659d2d9ac1112d85e006
Bruna <72671246+BrunaDev@users.noreply.github.com>
2023-05-28 21:44:16 -0300

    Update Main.java

--- Redes Sociais/src/br/inatel/cdg/Main.java
+++ Redes Sociais/src/br/inatel/cdg/Main.java
@@ -1,98 +1,135 @@
 package br.inatel.cdg;
-
 // Importações de classes de pacotes diferentes
 import br.inatel.cdg.redes.Facebook;
 import br.inatel.cdg.redes.GooglePlus;
 import br.inatel.cdg.redes.Instagram;
 import br.inatel.cdg.redes.Twitter;
+// Importação do Java
+import java.util.Scanner;
+//Importação de exceções
 import br.inatel.cdg.exception.ErroRedesSociais;
-
 
  public class Main {
 
-    public static String separator = "\n======================//====================\n"; // usado para criar um espaço no prompot para melhor
-                                                                                         // visualização de itens
+    public static String separator = "\n======================//====================\n"; // usado para criar um espaço no prompot para melhor visualização de itens
 
     public static void main(String[] args)  throws ErroRedesSociais{
 
+        RedeSocial[] redeSocial = new RedeSocial[5];
+
         //Criando o usuário
-        Usuario usuario = new Usuario();
+        Usuario usuario = new Usuario(redeSocial);
         usuario.setNome("Anakin");
         usuario.setEmail("anakin&padmeforever@gmail.com");
 
+        System.out.println(separator);
+
+        //Informando o nome de usuario e seu e-mail
+        System.out.println("Nome: " + usuario.getNome() + "\nE-mail: " + usuario.getEmail());
+        System.out.println(separator);
+
+        Scanner entrada = new Scanner(System.in);
+
         //Criando redes sociais
-        Facebook facebook = new Facebook();
-        facebook.senha = "123";
-        facebook.numAmigos = 1000;
+        for (int i = 0; i < redeSocial.length; i++) {
 
-        GooglePlus googleplus = new GooglePlus();
-        googleplus.senha = "456";
-        googleplus.numAmigos = 10;
+            int valor = 6;
 
-        Twitter twitter = new Twitter();
-        twitter.senha = "789";
-        twitter.numAmigos = 900;
+            System.out.println("Qual rede deseja acessar? - Entradas já programadas (senha e seguidores)");
 
-        Instagram instagram = new Instagram();
-        instagram.senha = "753";
-        instagram.numAmigos = 1200;
+            do {
+                System.out.println("1 - Facebook\n2 - Instagram\n3 - Google Plus\n4 - Twitter\n0 - Sair");
+                valor = entrada.nextInt();
+                entrada.nextLine();
 
-        //Setando o usuário cadastrado como o usuário das redes sociais
-        usuario.Usuario(facebook);
-        usuario.Usuario(googleplus);
-        usuario.Usuario(twitter);
-        usuario.Usuario(instagram);
+                if (!(valor == 1 || valor == 2 || valor == 3 || valor == 4 || valor == 0)) {
+                    System.out.println("Opção inválida. Digite novamente.");
+                }
+            
+            } while(!(valor == 1 || valor == 2 || valor == 3 || valor == 4 || valor == 0));
+            //Se os valores forem diferentes desses do while, volta para pergunta novamente.
+
+            //Se valor for igual a zero, interrompe o bloco de código
+            if(valor == 0) break;
+
+            //Verificação dos valores
+            switch (valor) {
+                case 1:  //facebook
+                    Facebook facebook = new Facebook("123", 2500);
+                    // Array de redeSociais na Classe Usuario recebe a rede social Facebook
+                    
+                    usuario.redeSocial[i] = facebook;
+                    break;
+                
+                case 2: //instagram
+                    Instagram instagram = new Instagram("753", 985);
+                    // Array de redeSociais na Classe Usuario recebe a rede social Instagram
+                    usuario.redeSocial[i] = instagram;
+                    break;
+
+                case 3: //google plus
+                    GooglePlus googleplus = new GooglePlus("456", 10);
+                    // Array de redeSociais na Classe Usuario recebe a rede social GooglePlus
+                    usuario.redeSocial[i] = googleplus;
+                    break;
+
+                case 4: //twitter
+                    Twitter twitter = new Twitter("789", 120);
+                    // Array de redeSociais na Classe Usuario recebe a rede social Twitter
+                    usuario.redeSocial[i] = twitter;
+                    break;
+
+                default:
+                    break;
+            }
+        }
 
         //Função para adicionar a separação/quebra de linha
         System.out.println(separator);
 
         //Tratamento de dados
-        for (int i = 0; i < usuario.redeSocial.length; i++) {
-            if (usuario.redeSocial[i] != null) {
+        for (int j = 0; j < usuario.redeSocial.length; j++) {
+            if (usuario.redeSocial[j] != null) {
                     //O operador instanceof é usado para verificar se um objeto é uma instância de uma determinada classe, uma instância de uma subclasse ou uma instância de uma classe que implementa uma determinada interface
-                    if (usuario.redeSocial[i] instanceof Facebook)
+                    if (usuario.redeSocial[j] instanceof Facebook)
                     {
-                       //Casting de facebook
-                        Facebook auxfb = (Facebook) usuario.redeSocial[i];
-                        auxfb.compartilhar();
-                        auxfb.postarFoto();
-                        auxfb.fazStreaming();
+                        System.out.println("\nFacebook:");
+                        usuario.redeSocial[j].postarComentario();
+                        usuario.redeSocial[j].postarFoto();
+                        usuario.redeSocial[j].postarVideo();
                         System.out.println(separator);
                     }
 
-                    else if (usuario.redeSocial[i] instanceof GooglePlus)
-                    {
-                       //Casting de google plus
-                        GooglePlus auxgp = (GooglePlus) usuario.redeSocial[i];
-                        auxgp.compartilhar();
-                        auxgp.fazStreaming();
-                        auxgp.curtirPublicacao();
+                    else if (usuario.redeSocial[j] instanceof GooglePlus)
+                    {   
+                        System.out.println("\nGoogle Plus:");
+                        ((GooglePlus) usuario.redeSocial[j]).fazStreaming();
+                        ((GooglePlus) usuario.redeSocial[j]).compartilhar();
+                        usuario.redeSocial[j].curtirPublicacao();
                         System.out.println(separator);
                     }
 
-                    else if (usuario.redeSocial[i] instanceof Twitter)
+                    else if (usuario.redeSocial[j] instanceof Twitter)
                     {
-                        //Casting de twitter
-                        Twitter auxtt = (Twitter) usuario.redeSocial[i];
-                        auxtt.compartilhar();
-                        auxtt.curtirPublicacao();
-                        auxtt.postarComentario();
+                        System.out.println("\nTwitter:");
+                        usuario.redeSocial[j].curtirPublicacao();
+                        usuario.redeSocial[j].postarFoto();
+                        ((Twitter) usuario.redeSocial[j]).compartilhar();
                         System.out.println(separator);
                     }
 
-                    else if (usuario.redeSocial[i] instanceof Instagram)
+                    else if (usuario.redeSocial[j] instanceof Instagram)
                     {
-                        //Casting de instagram
-                        Instagram auxinsta = (Instagram) usuario.redeSocial[i];
-                        auxinsta.postarComentario();
-                        auxinsta.postarVideo();
-                        auxinsta.postarFoto();
+                        System.out.println("\nInstragram:");
+                        usuario.redeSocial[j].postarFoto();
+                        usuario.redeSocial[j].postarVideo();
+                        usuario.redeSocial[j].curtirPublicacao();
                         System.out.println(separator);
                     }
                     else {
                         throw new ErroRedesSociais("Erro ao tratar dados das redes sociais");
                     }
-            }
+                }
         }
     }
 } 
